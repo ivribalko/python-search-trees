cbd35f3e6b7af9bebb3418ef6d8b7a84f4fa9f64
Ivan Rybalko <ivribalko@gmail.com>
2021-01-16 18:47:03 -0500

    assign tree

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import random
 import tree
 
+min_max = (0, 1000)
+
 
 def generate_numbers(count):
     for i in range(count):
-        yield random.randint(0, 1000)
+        yield random.randint(min_max[0], min_max[1])
 
 
-tree = tree.Tree()
+root = int(sum(min_max) / 2)
+data = tree.BinaryTree(root=tree.Node(root))
 
 for item in generate_numbers(count=10):
-    print(item)
+    data.add(tree.Node(item))
--- tree.py
+++ tree.py
@@ -1,3 +1,51 @@
-class Tree:
-    def __init__(self):
-        print('tree')
+class Node:
+    def __init__(self, key: int):
+        self._key = key
+        self._left = None
+        self._right = None
+
+    @property
+    def key(self) -> int:
+        return self._key
+
+    @property
+    def left(self):
+        return self._left
+
+    @left.setter
+    def left(self, value):
+        if self.left is not None:
+            raise NotImplementedError
+        self._left = value
+
+    @property
+    def right(self):
+        return self._right
+
+    @right.setter
+    def right(self, value):
+        if self.right is not None:
+            raise NotImplementedError
+        self._right = value
+
+
+class BinaryTree:
+    def __init__(self, root: Node):
+        self._root = root
+
+    def add(self, node: Node) -> None:
+        _add_recursive(parent=self._root, new_node=node)
+
+
+def _add_recursive(parent: Node, new_node: Node) -> None:
+    def _set_or_add(side: str) -> None:
+        node = getattr(parent, side)
+        if node is None:
+            setattr(parent, side, new_node)
+        else:
+            _add_recursive(parent=node, new_node=new_node)
+
+    if new_node.key <= parent.key:
+        _set_or_add(side='left')
+    else:
+        _set_or_add(side='right')
